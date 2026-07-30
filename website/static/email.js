692a08751640bdffe4b1358c76cd88d0c88dd842
niksaitov <nik.saitov.04@gmail.com>
2026-07-30 12:56:29 -0700

    refactor(website): remove flask app and python tooling

--- website/static/email.js
+++
@@ -1,8 +0,0 @@
-function sendEmailAdmin(){
-
-    const messageHead = 'mailto:nik.saitov.04@gmail.com?subject=Chess Website Feedback&body=';
-    const messageContent = document.getElementById("feedbackOutput").value;
-    const message = messageHead + messageContent;
-    window.location.href = message;
-    
-}
